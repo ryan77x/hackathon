83d9c1237859937908c92e1ada5786158cfe42d6
Ryan Liang <ryan7x7x@gmail.com>
2019-07-05 16:26:00 -0700

    initial setup codes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { component } from 'react';
+
+class App extends component{
+
+    render(){
+        return(
+            <h1>Hello World!!</h1>
+        );
+    }
+}
+
+export default App;
